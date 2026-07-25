b7a760ce4f6f397a1ef6b038eb18959c4c0e93cc
Randomfish123 <jonasrode12@gmail.com>
2026-07-25 12:38:36 +0200

    Added level 0 red Tilemap (not blue)

--- Scripts/shell.gd
+++ Scripts/shell.gd
@@ -8,7 +8,7 @@ extends RigidBody2D
 func _ready() -> void:
 	visible = true
 	lifetime.wait_time = randf_range(2.0, 7.0)
-	timer.wait_time = randf_range(0.0, 1.0)
+	timer.wait_time = randf_range(0.0, 0.5)
 	timer.start()
 	lifetime.start()
 
